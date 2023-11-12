0ddeaae77762da5e15e6ee0208c89ce823102f75
DaniilAliev <de.aliev@yandex.ru>
2023-11-12 17:09:27 +0300

    styles

--- src/components/Modal/WinModal.jsx
+++ src/components/Modal/WinModal.jsx
@@ -13,13 +13,16 @@ const WinModal = () => {
 
   return (
     <div>
-      <h1>{`Congrats! You won with ${stepsNumber} steps`}</h1>
-      <p>Хочешь сыграть снова?</p>
-      <div>
+      <h1>Поздравляем</h1>
+      <div className="text-container">
+        <p>{`Количество шагов: ${stepsNumber}`}</p>
+        <p>Сыграть еще раз?</p>
+      </div>
+      <div className="buttons">
         <button className="btn btn-restart" onClick={handleRestartButton}>
           Да, начать заново
         </button>
-        <button className="btn btn-exit" onClick={() => logOut()}>
+        <button className="btn btn-cancel" onClick={() => logOut()}>
           Выйти
         </button>
       </div>
